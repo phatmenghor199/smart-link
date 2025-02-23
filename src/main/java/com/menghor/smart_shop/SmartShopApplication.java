b93b144d891d5d65d7e867df4140f386c8a490f7
phatmenghor <phatmenghor19@gmail.com>
2025-02-23 22:03:26 +0700

    first

---
+++ src/main/java/com/menghor/smart_shop/SmartShopApplication.java
@@ -0,0 +1,13 @@
+package com.menghor.smart_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class git rSmartShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menghor/smart_shop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menghor.smart_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
