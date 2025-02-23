dceab033abc2ea73489ab8e1310a42be1d679cf1
phatmenghor <phatmenghor19@gmail.com>
2025-02-23 22:05:12 +0700

    first

--- src/main/java/com/menghor/smart_shop/SmartShopApplication.java
+++ src/main/java/com/menghor/smart_shop/SmartShopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class git rSmartShopApplication {
+public class SmartShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartShopApplication.class, args);
